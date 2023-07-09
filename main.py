d270e49374f8a7477b011da083487835dcd821b7
Vardhamann B E <rinzler@memex.local>
2023-07-09 19:18:44 +0530

    cleaned and now calculated TI

--- main.py
+++ main.py
@@ -1,40 +1,49 @@
 import pandas as pd
 import numpy as np
 
-def calculate_tactic_index(data):
+def calculate_final_xaxis_dist(data):
 
     # Initiate a counter
-    tactic_indices = {}
+    final_distances_xaxis = {}
 
     # Group the data by track number
     grouped_data = data.groupby('Track n°')
 
     # Loop through the tracks
     for track, track_data in grouped_data:
-        #print(track_data['X'].iloc[0])
-        # Calculate the distance between consecutive points
-        dx = track_data['X'].iloc[-1] - track_data['X'].iloc[0]
-        distances = (dx)
-        print(dx)
-        # Calculate the sum of forward distances and backward distances
-        forward_sum = np.sum(distances[distances > 0])
-        backward_sum = np.sum(distances[distances < 0])
-        # print(forward_sum)
-        # print(backward_sum)
-        # Calculate the tactic index
-        tactic_index = (forward_sum - backward_sum)/(forward_sum + backward_sum)
 
-        # Store the tactic index for the current track number
-        tactic_indices[track] = tactic_index
-        #print(tactic_index)
-    return tactic_indices
+        # Calculate the distance between consecutive points
+        dx = track_data['X'].iloc[23] - track_data['X'].iloc[0]
+
+        final_distances_xaxis[track] = dx
+        #print(dx)
+
+    return final_distances_xaxis
+
+def calc_tactic_index(data):
+
+    # Extract the second column values
+    second_column = [value for key, value in data.items()]
+
+    forward_sum = sum(1 for x in second_column if x > 0)
+    backward_sum = sum(1 for x in second_column if x < 0)
+
+    # Calculate the tactic index
+    tactic_index = (forward_sum - backward_sum)/(forward_sum + backward_sum)
+
+    return tactic_index
 
 # Load the tracked data from a CSV file
 tracked_data = pd.read_csv('./tracked_data.csv')
 
-# Calculate the tactic index for each track number
-tactic_indices = calculate_tactic_index(tracked_data)
+# Calculate the final distances for each track number
+final_distances_xaxis = calculate_final_xaxis_dist(tracked_data)
+# Calculate the Tactic index for 12 hour period
+final_ti = calc_tactic_index(final_distances_xaxis)
+
+# Print the final x distances for each track number
+for track, tactic_index in final_distances_xaxis.items():
+    print(f'Track {track} - X_f-X_i: {tactic_index}')
 
 # Print the tactic index for each track number
-for track, tactic_index in tactic_indices.items():
-    print(f'Track {track} - Tactic Index: {tactic_index}')
+print(f'Tactic index = {final_ti}')
