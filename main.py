ddbd904c96772f71d803b8dde7309278b3d6ba9b
Vardhamann B E <rinzler@memex.local>
2023-07-16 21:07:27 +0530

    removed print statements, added velocity and track_length

--- main.py
+++ main.py
@@ -23,6 +23,38 @@ def average_step_displacement(step_delta):
     return avg_positive, avg_negative
 
 
+def calculate_velocity(displacement, time):
+    return displacement / time
+
+
+def calculate_track_length(x, y):
+    distance = np.sqrt(np.diff(x)**2 + np.diff(y)**2)
+    return np.sum(distance)
+
+
+def analyze_tracks(tracked_data, output_file):
+    for track, track_data in tracked_data.groupby('Track no'):
+        dx = track_data['X'].iloc[26] - track_data['X'].iloc[0]
+        if dx > 0:
+            step_delta = np.diff(track_data['X'])
+            avg_positive_step_displacement, avg_negative_step_displacement = average_step_displacement(step_delta)
+
+            velocity_change = np.diff(track_data['X'])
+            velocity = calculate_velocity(dx, 12)  # Assuming 12 units of time
+
+            x = track_data['X']
+            y = track_data['Y']
+
+            track_length = calculate_track_length(x, y)
+
+            output_file.write(f'Track: {track}\n')
+            output_file.write(f'Avg +ve step displacement = {avg_positive_step_displacement}\n')
+            output_file.write(f'Avg -ve step displacement = {avg_negative_step_displacement}\n')
+            output_file.write(f'Velocity: {velocity}\n')
+            output_file.write(f'Velocity Change: {velocity_change}\n')
+            output_file.write(f'Track Length: {track_length}\n\n')
+
+
 input_folder_path = './input_files/'  # Replace with the path to your folder
 output_folder_path = './output_files/'  # Replace with the path to your output folder
 
@@ -35,26 +67,16 @@ with open(output_file_path, 'w') as output_file:
         file_path = os.path.join(input_folder_path, filename)
 
         if os.path.isfile(file_path) and filename.endswith('.csv'):
-            print(f'\nProcessing file: {file_path}\n')
             output_file.write(f'\nProcessing file: {file_path}\n\n')
 
             tracked_data = pd.read_csv(file_path)
             final_distances_xaxis = calculate_final_xaxis_dist(tracked_data)
             final_ti = calc_tactic_index(final_distances_xaxis)
 
-            print('Xf-Xi values for all tracks\n')
             output_file.write('Xf-Xi values for all tracks\n')
             for track, tactic_index in final_distances_xaxis.items():
-                print(f'Track {track} - X_f-X_i: {tactic_index}')
                 output_file.write(f'Track {track} - X_f-X_i: {tactic_index}\n')
 
-            print(f'\nTactic index = {final_ti}\n')
-            output_file.write(f'\nTactic index = {final_ti}\n')
+            output_file.write(f'\nTactic index = {final_ti}\n\n')
 
-            for track, track_data in tracked_data.groupby('Track no'):
-                dx = track_data['X'].iloc[26] - track_data['X'].iloc[0]
-                if dx > 0:
-                    step_delta = np.diff(track_data['X'])
-                    avg_positive_step_displacement, avg_negative_step_displacement = average_step_displacement(step_delta)
-                    print(f'track: {track},\n avg +ve step displacement = {avg_positive_step_displacement}\n avg -ve step displacement = {avg_negative_step_displacement}')
-                    output_file.write(f'track: {track},\n avg +ve step displacement = {avg_positive_step_displacement}\n avg -ve step displacement = {avg_negative_step_displacement}\n')
+            analyze_tracks(tracked_data, output_file)
